a1d11b6f216ac13230793860ca1e81601c58c6cc
Renato <renato.f.fabri@gmail.com>
2024-07-29 19:54:05 -0300

    Add backend request to NotesPage

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,7 +1,19 @@
-import { fakeData as notes } from "../assets/fakeData.js";
+import { useEffect, useState } from "react";
 import NoteCard from "../components/NoteCard.jsx";
+import { db } from "../appwrite/databases";
 
 function NotesPage() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    const response = await db.notes.list();
+    setNotes(response.documents);
+  };
+
   return (
     <div>
       {notes.map((note) => (
