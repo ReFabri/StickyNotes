6f1165dfa7e9361746eefcb2847d470fee5ddfb7
Renato <renato.f.fabri@gmail.com>
2024-08-07 20:33:35 -0300

    Color controls now change selectedNote color

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,8 +1,32 @@
 import { PropTypes } from "prop-types";
+import { useContext } from "react";
+import { NoteContext } from "../context/NoteContext";
+import { db } from "../appwrite/databases";
 
 function Color({ color }) {
+  const { selectedNote, notes, setNotes } = useContext(NoteContext);
+
   const changeColor = () => {
-    document.body.style.backgroundColor = color;
+    try {
+      const currentNoteIndex = notes.findIndex(
+        (note) => note.$id === selectedNote.$id
+      );
+
+      const updatedNote = {
+        ...notes[currentNoteIndex],
+        colors: JSON.stringify(color),
+      };
+
+      const newNotes = [...notes];
+      newNotes[currentNoteIndex] = updatedNote;
+      setNotes(newNotes);
+
+      db.notes.update(selectedNote.$id, {
+        colors: JSON.stringify(color),
+      });
+    } catch (error) {
+      alert("You must select a note before changing colors");
+    }
   };
 
   return (
